2bdeb5b94893ebc8d534a985b5be052c4fed2ca9
Vlad <aromudez@gmail.com>
2022-11-10 01:15:55 +0200

    changed styles for pagination

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Header from "./Header/Header";
 import PokemonsList from "./PokemonList/PokemonsList";
 import PokemonCard from "./PokemonCard/PokemonCard";
 
 const Pokemons: FC = () => {
+  const [choosenPokemonUrl, setUrl] = useState<string>("");
   return (
     <div className="app">
       <Header />
