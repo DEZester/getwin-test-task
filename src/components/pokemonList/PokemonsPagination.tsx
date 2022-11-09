6dbcc8437cd4ee772b5b10f6e0050527501995e1
Vlad <aromudez@gmail.com>
2022-11-09 12:11:16 +0200

    Created expressions for disabled btns\icons

--- src/components/pokemonList/PokemonsPagination.tsx
+++ src/components/pokemonList/PokemonsPagination.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 
 type Props = {
   nextPage?: () => void;
@@ -6,14 +6,33 @@ type Props = {
 };
 
 const PokemonsPagination: FC<Props> = ({ nextPage, prevPage }) => {
+  const btnPrevPageRef = useRef<HTMLButtonElement>(null);
+  const btnNextPageRef = useRef<HTMLButtonElement>(null);
+
+  const iconNextDisabled: string = btnNextPageRef.current?.disabled
+    ? "pagination__page-icon pagination__arrowNext pagination__arrowNext_disabled"
+    : "pagination__page-icon pagination__arrowNext";
+
+  const iconPrevDisabled: string = btnPrevPageRef.current?.disabled
+    ? "pagination__page-icon pagination__arrowBack pagination__arrowBack_disabled"
+    : "pagination__page-icon pagination__arrowBack";
+
   return (
     <div className="pagination">
-      <button className="pagination__page-btn" onClick={prevPage}>
-        <i className="pagination__page-icon pagination__arrowBack"></i>
+      <button
+        className="pagination__page-btn"
+        onClick={prevPage}
+        ref={btnPrevPageRef}
+      >
+        <i className={iconPrevDisabled}></i>
       </button>
       <span>Pagination</span>
-      <button className="pagination__page-btn" onClick={nextPage}>
-        <i className="pagination__page-icon pagination__arrowNext"></i>
+      <button
+        className="pagination__page-btn"
+        onClick={nextPage}
+        ref={btnNextPageRef}
+      >
+        <i className={iconNextDisabled}></i>
       </button>
     </div>
   );
