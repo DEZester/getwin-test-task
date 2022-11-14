e7a58899133b21930d9aa637117084e7a58e0c1e
Vlad <aromudez@gmail.com>
2022-11-14 02:02:47 +0200

    try and catch

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -6,19 +6,31 @@ const baseUrl: string =
 const typesUrl: string = "https://pokeapi.co/api/v2/type";
 
 export const fetchPokemonsList = async () => {
-  const response = await axios.get(baseUrl);
-  return response.data.results;
+  try {
+    const response = await axios.get(baseUrl);
+    return response.data.results;
+  } catch (error) {
+    alert(error);
+  }
 };
 
 export const fetchPokemon = async (pokemonUrl: string) => {
-  const response = await axios.get(pokemonUrl);
-  return response.data;
+  try {
+    const response = await axios.get(pokemonUrl);
+    return response.data;
+  } catch (error) {
+    alert(error);
+  }
 };
 
 export const fetchTypes = () =>
   axios.get(typesUrl).then((response) => response.data);
 
 export const fetchPokemonsListByType = async (pokemonUrl: string) => {
-  const response = await axios.get(pokemonUrl);
-  return response.data.pokemon;
+  try {
+    const response = await axios.get(pokemonUrl);
+    return response.data.pokemon;
+  } catch (error) {
+    alert(error);
+  }
 };
